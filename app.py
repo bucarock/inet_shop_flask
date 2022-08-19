d9fa85e0718e0d4cf2df95e98edb98461281eb51
Comp <buca-rock@mail.ru>
2022-08-19 16:10:57 +0300

    inet_shop_flask_1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from cloudipsp import Api, Checkout
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
@@ -13,6 +14,9 @@ class Item(db.Model):
     isActive = db.Column(db.Boolean, default=True)
     opisanie = db.Column(db.Text, nullable=False)
 
+    def __repr__(self):
+        return self.title
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -23,11 +27,37 @@ def about():
 
 @app.route('/catalog')
 def catalog():
-    return render_template('catalog.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('catalog.html', data=items)
 
-@app.route('/create')
+@app.route('/create', methods=['POST', 'GET'])
 def create():
-    return render_template('create.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+        opisanie = request.form['opisanie']
+        item = Item(title=title, price=price, opisanie=opisanie)
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/catalog')
+        except:
+            return "Произошла ошибка. попробуйте снова."
+    else:
+        return render_template('create.html')
+
+@app.route('/oplata/<int:id>')
+def oplata(id):
+    item = Item.query.get(id)
+    api = Api(merchant_id=1396424,
+              secret_key='test')
+    checkout = Checkout(api=api)
+    data = {
+        "currency": "RUB",
+        "amount": str(item.price) + "00"
+    }
+    url = checkout.url(data).get('checkout_url')
+    return redirect(url)
 
 if __name__ == '__main__':
     app.run(debug=True)
